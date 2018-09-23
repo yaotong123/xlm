fab525cb2468739bd0557d6da39b955da1c00725
yaotong123 <386805435@qq.com>
2018-09-23 09:40:22 +0800

    modify

---
+++ js/promise/promise.js
@@ -0,0 +1,159 @@
+/*粉色初识*/
+	$('#pink_title').animatext({
+		speed: 300,
+		effect: 'flipInY',
+		reverse: false,
+		initDelay:500
+	 });
+	  $('#pink_yt').delay(11000)
+	  $('#pink_yt').animate({
+	  	'opacity':'1'
+	  },8000)
+	  $('#pink_xlm').delay(16500)
+	  $('#pink_xlm').animate({
+	  	'opacity':'1'
+	  },5000)
+	  $('#pink_content1').animatext({
+		speed: 200,
+		effect: 'flipInY',
+		reverse: false,
+		initDelay:21000
+	 });
+	 $('#pink_content2').animatext({
+		speed: 300,
+		effect: 'flipInY',
+		reverse: false,
+		initDelay:23000
+	 });
+//	 $('#pink_like').animatext({
+//		speed: 800,
+//		effect: 'flipInY',
+//		reverse: false,
+//		initDelay:26000
+//	 });
+	 $('#pink_like').delay(26000)
+	  $('#pink_like').animate({
+	  	'opacity':'1'
+	  },3000)
+	  $('#pink').delay(29000)
+	  $('#pink').animate({
+	  	'opacity':'0'
+	  },4000)
+	  
+	 /*绿色萌芽*/
+	
+	$('#green_title').animatext({
+		speed: 300,
+		effect: 'flipInY',
+		reverse: false,
+		initDelay:31300
+	 });
+	 $('#green_yt').delay(37000)
+	  $('#green_yt').animate({
+	  	'opacity':'1'
+	  },5000)
+	$('#green_xlm').delay(41500)
+	  $('#green_xlm').animate({
+	  	'opacity':'1'
+	  },5000)
+	  $('#green_content').animatext({
+		speed: 300,
+		effect: 'flipInY',
+		reverse: false,
+		initDelay:44000
+	 });
+	 $('#green_like').delay(47000)
+	  $('#green_like').animate({
+	  	'opacity':'1'
+	  },3000)
+	  $('#green').delay(49000)
+	  $('#green').animate({
+	  	'opacity':'0'
+	  },4000)
+	/*//绿色萌芽*/
+	
+	/*蓝色相约*/
+	$('#blue_title').animatext({
+		speed: 300,
+		effect: 'flipInY',
+		reverse: false,
+		initDelay:51500
+	 });
+	 $('#blue_yt1').delay(56000)
+	  $('#blue_yt1').animate({
+	  	'opacity':'1'
+	  },8000)
+	  $('#blue_yt1_content').animatext({
+		speed: 300,
+		effect: 'flipInY',
+		reverse: false,
+		initDelay:59000
+	 });
+	 $('#blue_xlm1').delay(67000)
+	  $('#blue_xlm1').animate({
+	  	'opacity':'1'
+	  },2000)
+	  $('#blue_content2').animatext({
+		speed: 300,
+		effect: 'flipInY',
+		reverse: false,
+		initDelay:68000
+	 });
+	 $('#blue_ytA').delay(70000)
+	  $('#blue_ytA').animate({
+	  	'opacity':'0'
+	  },2000)
+	  $('#blue_xlm1').delay(70000)
+	  $('#blue_xlm1').animate({
+	  	'opacity':'0'
+	  },2000)
+	   $('#blue_content2').delay(70000)
+	  $('#blue_content2').animate({
+	  	'opacity':'0'
+	  },2000)
+	  $('#blue_xlm2').delay(71000)
+	  $('#blue_xlm2').animate({
+	  	'opacity':'1'
+	  },8000)
+	  $('#blue_content4').animatext({
+		speed: 300,
+		effect: 'flipInY',
+		reverse: false,
+		initDelay:73000
+	 });
+	 
+	 $('#blue_yt2').delay(76000)
+	  $('#blue_yt2').animate({
+	  	'opacity':'1'
+	  },8000)
+	   $('#blue_content3').animatext({
+		speed: 300,
+		effect: 'flipInY',
+		reverse: false,
+		initDelay:78000
+	 });
+	 $('#blue_like').delay(86000)
+	  $('#blue_like').animate({
+	  	'opacity':'1'
+	  },8000)
+	/*//蓝色相约*/
+	//	$("#pink_title").animatext();
+//	$("#pink_title").animatext({speed: 150,
+//		effect: 'flipInX',
+//		infinite: true
+//	});
+//	$("#pink_title").animatext({speed: 150,
+//		mode: "words",
+//		effect: 'swing',
+//		infinite: true
+//	});
+//	$("#pink_title").animatext({speed: 150,
+//		effect: 'tada',
+//		reverse: true,
+//		infinite: true
+//	});
+//	$("#pink_title").animatext({speed: 150,
+//		effect: 'tada',
+//		random: true,
+//		infinite: true
+//	});
